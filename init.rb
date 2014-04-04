6ef4cbb3db4c9ad4d7411537c4c803827b01bc67
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-04-04 14:38:01 +0900

    update urls

--- init.rb
+++ init.rb
@@ -3,8 +3,8 @@ Redmine::Plugin.register :set_rank_from_estimated_hours_in_issues do
   author 'Kazuhiro NISHIYAMA'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/znz/set_rank_from_estimated_hours_in_issues'
+  author_url 'https://github.com/znz'
 end
 
 require_relative 'lib/issue_hooks'
